59660496b4179384de01dea20d697e289b2ec282
gswirrl <guy@swirrl.com>
2024-01-28 08:29:54 +0000

    changing config doesnt fix error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+/*
+
+per https://stackoverflow.com/questions/64639839/typescript-webpack-library-generates-referenceerror-self-is-not-defined
+
+this is intended to fix the compilation issue.
+
+i think it is correct by https://webpack.js.org/configuration/output/#outputglobalobject
+
+but as you can see, it doesnt fix the problem.
+
+*/
+
+
+
+const nextConfig = {
+	webpack: (
+    	config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+  config.output.globalObject = 'this'
+    // Important: return the modified config
+    return config
+  },
+};
+
 
 export default nextConfig;
