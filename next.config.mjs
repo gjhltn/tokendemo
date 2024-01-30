54b85271f6bac4b053fab430f52e8d75ee26f074
gswirrl <guy@swirrl.com>
2024-01-30 03:58:07 +0000

    clean next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,3 @@
-/** @type {import('next').NextConfig} */
-
-/*
-
-per https://stackoverflow.com/questions/64639839/typescript-webpack-library-generates-referenceerror-self-is-not-defined
-
-this is intended to fix the compilation issue.
-
-i think it is correct by https://webpack.js.org/configuration/output/#outputglobalobject
-
-but as you can see, it doesnt fix the problem.
-
-*/
-
-
 
 const nextConfig = {
 };
